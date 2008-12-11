c00604be0af492cab94128710c3e860a1ae81392
Lance Carlson <lancecarlson@gmail.com>
2008-12-11 02:49:23 -0500

    Fixed all specs

--- spec/lib/weekdays_spec.rb
+++ spec/lib/weekdays_spec.rb
@@ -8,7 +8,7 @@ describe CChronic::Weekdays do
   it "should return the time for each day" do
     i = 0
     %w(sunday monday tuesday wednesday thursday friday saturday).each do |day|
-      time = Weekdays.time_for_day(day)
+      time = CChronic::Weekdays.time_for_day(day)
       time.wday.should == i
       %w(year month hour min sec).each do |a|
         time.send(a).should == @now.send(a)
@@ -20,16 +20,12 @@ describe CChronic::Weekdays do
   it "should return the time for each short day" do
     i = 0
     %w(sun mon tue wed thu fri sat).each do |day|
-      time = Weekdays.time_for_day(day)
+      time = CChronic::Weekdays.time_for_day(day)
       time.wday.should == i
       %w(year month hour min sec).each do |a|
         time.send(a).should == @now.send(a)
       end
       i += 1
     end
-  end
-  
-  it "should return the time for the beginning of the week" do
-    @weekday.beginning_of_week.wday.should == 0
   end
 end
